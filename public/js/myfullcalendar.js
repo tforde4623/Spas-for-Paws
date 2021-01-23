64447684a99daaaac2829a09c04438db8f7f29db
smary <sziebe@gmail.com>
2021-01-23 17:07:07 -0600

    lint fixes

--- public/js/myfullcalendar.js
+++ public/js/myfullcalendar.js
@@ -43,26 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
     modal.addClass("is-active");
   }
 
-  function formatDate(date) {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    const ampm = hours >= 12 ? "pm" : "am";
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    const strTime = hours + ":" + minutes + " " + ampm;
-    return (
-      date.getMonth() +
-      1 +
-      "/" +
-      date.getDate() +
-      "/" +
-      date.getFullYear() +
-      "  " +
-      strTime
-    );
-  }
-
   const calendarEl = document.getElementById("calendar");
   const calendar = new FullCalendar.Calendar(calendarEl, {
     headerToolbar: {
