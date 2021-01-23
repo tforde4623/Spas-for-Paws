639512f3fc8ab19fd5584e84339796e6b1b1c27d
smary <sziebe@gmail.com>
2021-01-23 14:06:41 -0600

    bug fixes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // can use some of this login for checking if logged in
   // app.get("/", (req, res) => {
   //   // If the user already has an account send them to the members page
@@ -13,7 +13,7 @@ module.exports = function (app) {
   // });
 
   // index/home page route
-  app.get("/", res => {
+  app.get("/", (res) => {
     // replace with render
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
@@ -40,7 +40,7 @@ module.exports = function (app) {
   //   res.sendFile(path.join(__dirname, "../public/members.html"));
   // });
 
-  app.get("/calendar", checkAuthentication, (req,res) => {
+  app.get("/calendar", checkAuthentication, (req, res) => {
     //render calendar page via handlebars IF logged in
     res.render("calendar", { dataIn: serviceData });
   });
@@ -60,63 +60,122 @@ const serviceData = [
     title: "Bath & Full Haircut",
     description: "For dogs who need a bath & haircut.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Bath & Brush",
     description:
       "For dogs who just need a bath to maintain a healthy-looking coat, clean ears & trimmed nails.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Puppy Bath & Trim",
     description:
       "For puppies through 5 months old who need a bath & light trimming, plus TLC to help them get comfortable visiting the salon.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Puppy Bath & Brush",
     description:
       "For puppies through 5 months old who need a bath to maintain a healthy-looking coat, plus extra pampering. Plus, our pet-loving stylists help get puppy comfortable visiting the salon.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Nail Trim",
     description: "Prevents painful splaying & splitting of your dog’s nails.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Flea and Tick Treatment",
     description:
       "Our all-natural flea and tick shampoos work safely with Frontline or other similar preventative applications.",
     animal: "Dog",
+    isDog: true,
+    isCat: false,
+    isRabbit: false,
   },
   {
     title: "Bath & Full Haircut",
     description: "For cats who need a bath & haircut.",
     animal: "Cat",
+    isDog: false,
+    isCat: true,
+    isRabbit: false,
   },
   {
     title: "Bath & Brush",
     description:
       "For cats who need just a bath for healthy-looking coat, clean ears & trimmed nails.",
     animal: "Cat",
+    isDog: false,
+    isCat: true,
+    isRabbit: false,
   },
   {
     title: "Kitten Bath & Trim",
     description:
       "For kittens through 5 months old who need a bath & haircut. Plus our pet stylists help get them comfortable visiting the salon.",
     animal: "Cat",
+    isDog: false,
+    isCat: true,
+    isRabbit: false,
   },
   {
     title: "Kitten Bath & Brush",
     description:
       "For kittens through 5 months old who need a bath to maintain a healthy-looking coat, clean ears & trimmed nails. Plus, our pet-loving stylists help get kittens comfortable visiting the salon.",
     animal: "Cat",
+    isDog: false,
+    isCat: true,
+    isRabbit: false,
   },
   {
     title: "Nail Trim",
     description: "Prevents painful splaying & splitting of your cat's nails.",
     animal: "Cat",
+    isDog: false,
+    isCat: true,
+    isRabbit: false,
+  },
+  {
+    title: "De-matting & Gland Cleaning",
+    description: "You bunny will be soft and fluffy!",
+    animal: "Rabbit",
+    isDog: false,
+    isCat: false,
+    isRabbit: true,
+  },
+  {
+    title: "Nail Trim and File",
+    description:
+      "Prevents painful splaying & splitting of your rabbit's nails.",
+    animal: "Rabbit",
+    isDog: false,
+    isCat: false,
+    isRabbit: true,
+  },
+  {
+    title: "Bunny Portrait",
+    description:
+      "A delighful pet portrait of your bunnie for special occasions! ",
+    animal: "Rabbit",
+    isDog: false,
+    isCat: false,
+    isRabbit: true,
   },
 ];
 
